b21b1ec2f3ab3dd6599bb1e1d004c1b2647462da
mailitadarsh-maker <mailitadarsh@gmail.com>
2026-06-03 15:37:30 +0530

    Add incidents page

---
+++ app/incidents/page.tsx
@@ -0,0 +1,9 @@
+export default function IncidentsPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        Incident History
+      </h1>
+    </div>
+  );
+}
